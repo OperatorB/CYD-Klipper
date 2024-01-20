7418738fe76dd60dc26be8dad5ba3a841c14c28f
OperatorB <34603896+OperatorB@users.noreply.github.com>
2024-01-20 16:07:41 +0100

    Update screen_driver.cpp
    remove XPT2048 functions, add GT911 and RGB led functions

--- CYD-Klipper/src/core/screen_driver.cpp
+++ CYD-Klipper/src/core/screen_driver.cpp
@@ -4,8 +4,7 @@
 #include "../conf/global_config.h"
 #include "lvgl.h"
 
-SPIClass touchscreen_spi = SPIClass(HSPI);
-XPT2046_Touchscreen touchscreen(XPT2046_CS, XPT2046_IRQ);
+TAMC_GT911 tp = TAMC_GT911(TOUCH_SDA, TOUCH_SCL, TOUCH_INT, TOUCH_RST, TOUCH_WIDTH, TOUCH_HEIGHT);
 
 uint32_t LV_EVENT_GET_COMP_CHILD;
 
@@ -17,78 +16,9 @@ TFT_eSPI tft = TFT_eSPI();
 bool isScreenInSleep = false;
 lv_timer_t *screenSleepTimer;
 
-TS_Point touchscreen_point()
-{
-    TS_Point p = touchscreen.getPoint();
-    p.x = round((p.x * global_config.screenCalXMult) + global_config.screenCalXOffset);
-    p.y = round((p.y * global_config.screenCalYMult) + global_config.screenCalYOffset);
-    return p;
-}
-
-void touchscreen_calibrate(bool force)
-{
-    if (global_config.screenCalibrated && !force)
-        {
-            return;
-        }
-
-    tft.fillScreen(TFT_BLACK);
-    tft.setCursor(20, 140);
-    tft.setTextColor(TFT_WHITE, TFT_BLACK);
-    tft.setTextSize(2);
-    tft.println("Calibrate Screen");
-
-    TS_Point p;
-    int16_t x1, y1, x2, y2;
-
-    while (touchscreen.touched())
-        ;
-    tft.drawFastHLine(0, 10, 20, ILI9341_WHITE);
-    tft.drawFastVLine(10, 0, 20, ILI9341_WHITE);
-    while (!touchscreen.touched())
-        ;
-    delay(50);
-    p = touchscreen.getPoint();
-    x1 = p.x;
-    y1 = p.y;
-    tft.drawFastHLine(0, 10, 20, ILI9341_BLACK);
-    tft.drawFastVLine(10, 0, 20, ILI9341_BLACK);
-    delay(500);
-
-    while (touchscreen.touched())
-        ;
-    tft.drawFastHLine(300, 230, 20, ILI9341_WHITE);
-    tft.drawFastVLine(310, 220, 20, ILI9341_WHITE);
-
-    while (!touchscreen.touched())
-        ;
-    delay(50);
-    p = touchscreen.getPoint();
-    x2 = p.x;
-    y2 = p.y;
-    tft.drawFastHLine(300, 230, 20, ILI9341_BLACK);
-    tft.drawFastVLine(310, 220, 20, ILI9341_BLACK);
-
-    int16_t xDist = 320 - 40;
-    int16_t yDist = 240 - 40;
-
-    global_config.screenCalXMult = (float)xDist / (float)(x2 - x1);
-    global_config.screenCalXOffset = 20.0 - ((float)x1 * global_config.screenCalXMult);
-
-    global_config.screenCalYMult = (float)yDist / (float)(y2 - y1);
-    global_config.screenCalYOffset = 20.0 - ((float)y1 * global_config.screenCalYMult);
-
-    global_config.screenCalibrated = true;
-    WriteGlobalConfig();
-}
-
 void screen_setBrightness(byte brightness)
 {
-    // calculate duty, 4095 from 2 ^ 12 - 1
-    uint32_t duty = (4095 / 255) * brightness;
-
-    // write duty to LEDC
-    ledcWrite(0, duty);
+    analogWrite(TFT_BL, brightness);
 }
 
 void set_screen_brightness()
@@ -104,6 +34,7 @@ void screen_timer_wake()
     lv_timer_reset(screenSleepTimer);
     isScreenInSleep = false;
     set_screen_brightness();
+    set_LED_color(global_config.LEDcolors);
 
     // Reset cpu freq
     setCpuFrequencyMhz(CPU_FREQ_HIGH);
@@ -114,6 +45,14 @@ void screen_timer_sleep(lv_timer_t *timer)
 {
     screen_setBrightness(0);
     isScreenInSleep = true;
+    uint8_t dimVal[3] = {0, 0, 0};
+    if (global_config.LEDcolors[0] > 0)
+        dimVal[0] = global_config.LEDcolors[0] * 0.1;
+    if (global_config.LEDcolors[1] > 0)
+        dimVal[1] = global_config.LEDcolors[1] * 0.1;
+    if (global_config.LEDcolors[2] > 0)
+        dimVal[2] = global_config.LEDcolors[2] * 0.1;
+    set_LED_color(dimVal);
 
     // Screen is off, no need to make the cpu run fast, the user won't notice ;)
     setCpuFrequencyMhz(CPU_FREQ_LOW);
@@ -161,42 +100,49 @@ void screen_lv_flush(lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *col
 
 void screen_lv_touchRead(lv_indev_drv_t *indev_driver, lv_indev_data_t *data)
 {
-
-    if (touchscreen.tirqTouched() && touchscreen.touched())
-    {
-        lv_timer_reset(screenSleepTimer);
-        // dont pass first touch after power on
-        if (isScreenInSleep)
-        {
-            screen_timer_wake();
-            while (touchscreen.touched())
-                ;
-            return;
-        }
-
-        TS_Point p = touchscreen_point();
-        data->state = LV_INDEV_STATE_PR;
-        data->point.x = p.x;
-        data->point.y = p.y;
-    }
-    else
+    tp.read();
+    if (!tp.isTouched)
     {
         data->state = LV_INDEV_STATE_REL;
     }
+    else
+    {
+        data->state = LV_INDEV_STATE_PR;
+        for (int i = 0; i < tp.touches; i++)
+        {
+            uint16_t magicX = TOUCH_HEIGHT - tp.points[i].y; // fix GT911 driver - orientation
+            uint16_t magicY = tp.points[i].x;
+
+            data->point.x = magicX;
+            data->point.y = magicY;
+
+            // Serial.print("Touch ");Serial.print(i+1);Serial.print(": ");
+            // Serial.print("  x: ");Serial.print(magicX);
+            // Serial.print("  y: ");Serial.print(magicY);
+            // Serial.print("  size: ");Serial.println(tp.points[i].size);
+        }
+        lv_timer_reset(screenSleepTimer);
+        if (isScreenInSleep)
+            screen_timer_wake();
+    }
 }
 
-void set_color_scheme(){
+void set_color_scheme()
+{
     lv_disp_t *dispp = lv_disp_get_default();
     lv_color_t main_color = {0};
     COLOR_DEF color_def = color_defs[global_config.color_scheme];
 
-    if (color_defs[global_config.color_scheme].primary_color_light > 0){
+    if (color_defs[global_config.color_scheme].primary_color_light > 0)
+    {
         main_color = lv_palette_lighten(color_def.primary_color, color_def.primary_color_light);
     }
-    else if (color_defs[global_config.color_scheme].primary_color_light < 0) {
+    else if (color_defs[global_config.color_scheme].primary_color_light < 0)
+    {
         main_color = lv_palette_darken(color_def.primary_color, color_def.primary_color_light * -1);
     }
-    else {
+    else
+    {
         main_color = lv_palette_main(color_defs[global_config.color_scheme].primary_color);
     }
 
@@ -204,36 +150,42 @@ void set_color_scheme(){
     lv_disp_set_theme(dispp, theme);
 }
 
-void set_invert_display(){
+void set_invert_display()
+{
     tft.invertDisplay(global_config.invertColors);
+}
+
+void LED_init()
+{
+    pinMode(LED_PIN_R, OUTPUT);
+    pinMode(LED_PIN_G, OUTPUT);
+    pinMode(LED_PIN_B, OUTPUT);
+    set_LED_color(global_config.LEDcolors);
+}
+
+void set_LED_color(uint8_t rgbVal[3])
+{
+    analogWrite(LED_PIN_R, 255 - rgbVal[0]);
+    analogWrite(LED_PIN_G, 255 - rgbVal[1]);
+    analogWrite(LED_PIN_B, 255 - rgbVal[2]);
 }
 
 void screen_setup()
 {
-    touchscreen_spi.begin(XPT2046_CLK, XPT2046_MISO, XPT2046_MOSI, XPT2046_CS);
-    touchscreen.begin(touchscreen_spi);
-    touchscreen.setRotation(global_config.rotateScreen ? 3 : 1);
-
+    // Initialize the touchscreen
+    tp.begin();
+    tp.setRotation(ROTATION_NORMAL);
+    // Initialize LVGL
     lv_init();
-
+    // Initialize the display
     tft.init();
-
-    ledcSetup(0, 5000, 12);
-    ledcAttachPin(21, 0);
-
     tft.setRotation(global_config.rotateScreen ? 3 : 1);
     tft.fillScreen(TFT_BLACK);
     set_screen_brightness();
     set_invert_display();
-
-    touchscreen_spi.begin(XPT2046_CLK, XPT2046_MISO, XPT2046_MOSI, XPT2046_CS);
-    touchscreen.begin(touchscreen_spi);
-
-    touchscreen_calibrate();
+    LED_init();
 
     lv_disp_draw_buf_init(&draw_buf, buf, NULL, TFT_WIDTH * TFT_HEIGHT / 10);
-
-    /*Initialize the display*/
     static lv_disp_drv_t disp_drv;
     lv_disp_drv_init(&disp_drv);
     disp_drv.hor_res = TFT_HEIGHT;
@@ -242,18 +194,18 @@ void screen_setup()
     disp_drv.draw_buf = &draw_buf;
     lv_disp_drv_register(&disp_drv);
 
-
-    /*Initialize the (dummy) input device driver*/
+    // Initialize the (dummy) input device driver
     static lv_indev_drv_t indev_drv;
     lv_indev_drv_init(&indev_drv);
     indev_drv.type = LV_INDEV_TYPE_POINTER;
     indev_drv.read_cb = screen_lv_touchRead;
     lv_indev_drv_register(&indev_drv);
 
+    // Initialize screen dimming timer
     screen_timer_setup();
     screen_timer_start();
 
-    /*Initialize the graphics library */
+    // Initialize the graphics library
     LV_EVENT_GET_COMP_CHILD = lv_event_register_id();
     set_color_scheme();
 }
